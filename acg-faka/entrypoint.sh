8149d0db4212e3eaeec0fb237cf04435e61715af
monlor <me@monlor.com>
2024-03-18 17:48:21 +0800

    fix: 🐛 修复k8s部署失败的情况

--- acg-faka/entrypoint.sh
+++ acg-faka/entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-if [ -z "$(ls -A /var/www/html)" ]; then
+# 如果文件数量小于2，则安装，可能存在lost+found这个文件
+if [ "$(ls -A /var/www/html | wc -l)" -lt 2 ]; then
     echo "安装acg-faka程序..."
     cp -rf /tmp/acg-faka-main/. /var/www/html
 fi
