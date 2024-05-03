3d9e62b71c6d511bd1d35d7959fa1e3c6f747878
monlor <me@monlor.com>
2024-05-04 01:19:02 +0800

    feat: 🎸 持久化env

--- dujiaoka/entrypoint.sh
+++ dujiaoka/entrypoint.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+set -e
+
 export INSTALL=false
 
-sed -i "s/^DB_HOST=.*/DB_HOST=${DB_HOST}/g" /dujiaoka/.env
-sed -i "s/^DB_PORT=.*/DB_PORT=${DB_PORT:-3306}/g" /dujiaoka/.env
-sed -i "s/^DB_DATABASE=.*/DB_DATABASE=${DB_DATABASE}/g" /dujiaoka/.env
-sed -i "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USERNAME}/g" /dujiaoka/.env
-sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/g" /dujiaoka/.env
+if [ ! -f /dujiaoka/storage/.env ]; then
+    cp -a /dujiaoka/.env /dujiaoka/storage/.env
+fi
 
-sed -i "s/^REDIS_HOST=.*/REDIS_HOST=${REDIS_HOST}/g" /dujiaoka/.env
-sed -i "s/^REDIS_PASSWORD=.*/REDIS_PASSWORD=${REDIS_PASSWORD}/g" /dujiaoka/.env
-sed -i "s/^REDIS_PORT=.*/REDIS_PORT=${REDIS_PORT:-6379}/g" /dujiaoka/.env
+sed -i "s/^DB_HOST=.*/DB_HOST=${DB_HOST}/g" /dujiaoka/storage/.env
+sed -i "s/^DB_PORT=.*/DB_PORT=${DB_PORT:-3306}/g" /dujiaoka/storage/.env
+sed -i "s/^DB_DATABASE=.*/DB_DATABASE=${DB_DATABASE}/g" /dujiaoka/storage/.env
+sed -i "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USERNAME}/g" /dujiaoka/storage/.env
+sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/g" /dujiaoka/storage/.env
 
-sed -i "s/^CACHE_DRIVER=.*/CACHE_DRIVER=${CACHE_DRIVER:-redis}/g" /dujiaoka/.env
+sed -i "s/^REDIS_HOST=.*/REDIS_HOST=${REDIS_HOST}/g" /dujiaoka/storage/.env
+sed -i "s/^REDIS_PASSWORD=.*/REDIS_PASSWORD=${REDIS_PASSWORD}/g" /dujiaoka/storage/.env
+sed -i "s/^REDIS_PORT=.*/REDIS_PORT=${REDIS_PORT:-6379}/g" /dujiaoka/storage/.env
+
+sed -i "s/^CACHE_DRIVER=.*/CACHE_DRIVER=${CACHE_DRIVER:-redis}/g" /dujiaoka/storage/.env
 
 if [ ! -f /dujiaoka/.install.lock ]; then
     echo "启用初始化安装模式..."
     export INSTALL=true
     touch /dujiaoka/.install.lock
 fi
+
+ln -sf /dujiaoka/storage/.env /dujiaoka/.env
 
 /opt/start.sh
