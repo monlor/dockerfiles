910257bde6ed7454899ea557e88db21b6e96ef4d
monlor <me@monlor.com>
2024-07-09 15:17:16 +0800

    feat: 🎸 重构epay的部署方式

--- epay/start.sh
+++ epay/start.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-if [ -d /tmp/epay ]; then
-    echo "更新epay文件..."
-    if [ -f /var/www/html/install/install.lock ]; then
-        echo "检测到历史文件，恢复中..."
-        cp -rf /var/www/html/install/install.lock /tmp/epay/install
-        cp -rf /var/www/html/config.php /tmp/epay/config.php
-    fi
-    rsync -av --delete /tmp/epay/ /var/www/html/ &> /dev/null
-    rm -rf /tmp/epay
-fi
+# 先更新安装文件
+cp -rf /var/www/html/install/ /data/install/
+
+# 软连接安装文件，保存install.lock
+ln -sf /data/install /var/www/html/install
+
+cat > /var/www/html/config.php <<-EOF
+<?php
+    /*数据库配置*/
+    \$dbconfig=array(
+        'host' => '${DB_HOST}', //数据库服务器
+        'port' => ${DB_PORT}, //数据库端口
+        'user' => '${DB_USERNAME}', //数据库用户名
+        'pwd' => '${DB_PASSWORD}', //数据库密码
+        'dbname' => '${DB_DATABASE}', //数据库名
+        'dbqz' => 'pay' //数据表前缀
+    );
+EOF
 
 apache2-foreground
