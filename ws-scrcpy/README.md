d269c8b0af3226bd659d0d220ce01bfabb7238fd
monlor <me@monlor.com>
2024-07-24 13:14:57 +0800

    feat: 🎸 支持自动配置adb

--- ws-scrcpy/README.md
+++ ws-scrcpy/README.md
@@ -2,6 +2,18 @@
 
 web版本的scrapy，用于安卓远程控制，适配arm64/amd64
 
+## 部署
+
+```bash
+docker run -d --name ws-scrcpy -p 8000:8000 -e ADB_ADDRESS=xx monlor/ws-scrcpy
+```
+
+## 环境变量
+
+`ADB_ADDRESS`: adb连接地址，支持多个，格式：xxx.com:5555,aaa.com:5555
+
+`TIMEOUT`: adb地址连接超时时间，默认60s
+
 # 参考
 
 https://github.com/scavin/ws-scrcpy-docker
---
+++ ws-scrcpy/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+TIMEOUT=${TIMEOUT:-60}
+
+if [ -n "${ADB_ADDRESS:-}" ]; then
+  echo ${ADB_ADDRESS} | tr ',' '\n' | while read address; do
+    echo "Connect to ${address} ..."
+    host=$(echo ${address} | cut -d':' -f1)
+    port=$(echo ${address} | cut -d':' -f2)
+    start=0
+    while ! nc -z "${host}" "${port}" &> /dev/null; do
+      echo "Failed to connect to ${address}, retry after 1s ..."
+      sleep 1
+      start=$((start+1))
+      if [ "${start}" -ge "${TIMEOUT}" ]; then
+        echo "Timeout to connect to ${address}, break ..."
+        break
+      fi
+    done
+    if [ "${start}" -ge "${TIMEOUT}" ]; then
+        echo "Skipping to next address ..."
+        continue
+    fi
+    adb connect "${address}"
+  done
+fi
+
+node dist/index.js
