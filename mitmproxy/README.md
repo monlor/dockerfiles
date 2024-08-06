da0b503d023239280eed277e2f248c49555c53fb
monlor <me@monlor.com>
2024-08-06 18:21:32 +0800

    feat: 🎸 block local ip access

--- mitmproxy/README.md
+++ mitmproxy/README.md
@@ -5,6 +5,7 @@ export MITMPROXY_USER=your_username
 export MITMPROXY_PASS=your_password
 export REMOTE_SCRIPT_URL=http://example.com/your_remote_script.py
 export SCRIPT_UPDATE_INTERVAL=300  # 可选，默认为300秒
+export LOG_LEVEL=error
 ```
 
 ## Volume
@@ -15,9 +16,9 @@ export SCRIPT_UPDATE_INTERVAL=300  # 可选，默认为300秒
 
 ## Port
 
-ui: 80,8080
+ui: 81
 
-proxy: 8081
+proxy: 80
 
 ## Rmote script
 
--- mitmproxy/entrypoint.sh
+++ mitmproxy/entrypoint.sh
@@ -5,7 +5,7 @@ export MITMPROXY_USER=${MITMPROXY_USER:-""}
 export MITMPROXY_PASS=${MITMPROXY_PASS:-""}
 
 # 构建 mitmweb 命令
-MITMWEB_CMD="mitmweb --web-host 0.0.0.0 --web-port 8081 --listen-port 8080 -s /mitmproxy.py $@"
+MITMWEB_CMD="mitmweb --web-host 0.0.0.0 --set console_eventlog_verbosity=${LOG_LEVEL:-error} --web-port 8081 --listen-port 8080 -s /mitmproxy.py $@"
 
 # 如果设置了用户名和密码，添加代理认证
 if [ -n "$MITMPROXY_USER" ] && [ -n "$MITMPROXY_PASS" ]; then
