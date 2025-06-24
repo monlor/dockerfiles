e96f9b093950a2f0a5261346fdafc98abfd26181
monlor <20394007+monlor@users.noreply.github.com>
2025-06-24 23:57:14 +0800

    feat: 🎸 Independent SSH PUBLIC KEY configuration

--- coder-development/entrypoint.sh
+++ coder-development/entrypoint.sh
@@ -10,24 +10,26 @@ fi
 sudo chown coder:coder /home/coder
 sudo chmod 755 /home/coder
 
+# setup ssh
+if [ ! -d ~/.ssh ]; then
+  mkdir -p ~/.ssh
+  chmod 700 ~/.ssh
+fi
+if [ ! -f ~/.ssh/ssh_host_rsa_key ]; then
+  ssh-keygen -t rsa -f ~/.ssh/ssh_host_rsa_key -N ''
+fi
+if [ ! -f ~/.ssh/ssh_host_ecdsa_key ]; then
+  ssh-keygen -t ecdsa -f ~/.ssh/ssh_host_ecdsa_key -N ''
+fi
+if [ ! -f ~/.ssh/ssh_host_ed25519_key ]; then
+  ssh-keygen -t ed25519 -f ~/.ssh/ssh_host_ed25519_key -N ''
+fi
+chmod 600 ~/.ssh/ssh_host_*
+sudo /usr/sbin/sshd
+
 if [ -n "${SSH_PUBLIC_KEY}" ]; then
-  if [ ! -d ~/.ssh ]; then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-  fi
   echo "${SSH_PUBLIC_KEY}" > ~/.ssh/authorized_keys
   chmod 600 ~/.ssh/authorized_keys
-  if [ ! -f ~/.ssh/ssh_host_rsa_key ]; then
-    ssh-keygen -t rsa -f ~/.ssh/ssh_host_rsa_key -N ''
-  fi
-  if [ ! -f ~/.ssh/ssh_host_ecdsa_key ]; then
-    ssh-keygen -t ecdsa -f ~/.ssh/ssh_host_ecdsa_key -N ''
-  fi
-  if [ ! -f ~/.ssh/ssh_host_ed25519_key ]; then
-    ssh-keygen -t ed25519 -f ~/.ssh/ssh_host_ed25519_key -N ''
-  fi
-  chmod 600 ~/.ssh/ssh_host_*
-  sudo /usr/sbin/sshd
 fi
 
 /tmp/code-server/bin/code-server --port 8080 --host 0.0.0.0
